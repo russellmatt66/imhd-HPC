b4c2f2ef0025857dd40799014b8b4f0c8392ca8f
russellmatt66 <47687321+russellmatt66@users.noreply.github.com>
2023-10-27 23:23:32 -0700

    Update numerics.hpp
    Periodic BCs implemented

--- cpp/include/numerics.hpp
+++ cpp/include/numerics.hpp
@@ -70,16 +70,71 @@ void MacCormackAdvance(imhdFluid& imhdFluid, const double dt, const double dx){
 
 // Periodic Boundary Conditions - Separation of Concerns
 void PeriodicBCs(imhdFluid& imhdFluid){
-            // Boundary conditions:
-            // (1) Periodic in z (excluding the perimeter of the xy-planes at k = k_{max} = N - 1)
-            //      - Q(i,j,0) = Q(i,j,N-1)
-            //      - Implement: Q(i,j,0) += Q(i,j,N-1); Q(i,j,N-1) = Q(i,j,0)
-            // (2) Rigid, perfectly conducting wall surrounding the fusion plasma: 
-            //  - 2 yz-planes: (0,y,z); (N-1,y,z);
-            //  - 2 xz-planes: (x,0,z); (x,N-1,z);
-            // Doing BCs like this should have negligible performance implications: O(N^2) work compared to O(N^3) when computing the interior points
-            // Come back to this during benchmarking
+    // Boundary conditions:
+    // (1) Periodic in z (excluding the perimeter of the xy-planes at k = k_{max} = N - 1)
+    //      - Q(i,j,0) = Q(i,j,N-1)
+    //      - Implement: Q(i,j,0) += Q(i,j,N-1); Q(i,j,N-1) = Q(i,j,0)
+    // (2) Rigid, perfectly conducting wall surrounding the fusion plasma: 
+    //  - 2 yz-planes: (0,y,z); (N-1,y,z);
+    //  - 2 xz-planes: (x,0,z); (x,N-1,z);
+    // Doing BCs like this should have negligible performance implications: O(N^2) work compared to O(N^3) when computing the interior points
+    // Come back to this statement during benchmarking
+    size_t N = imhdFluid.getSideLen(), numVars = imhdFluid.getNumVars();
+    double gamma = imhdFluid.getGamma();
+    
+    // k = 0 and k = N-1 xy-planes
+    for (size_t iv = 0; iv < numVars; iv++){
+        for (size_t i = 0; i < N; i++){
+            for (size_t j = 0; j < N; j++){
+                imhdFluid.imhdVar(iv,i,j,0) += imhdFluid.imhdVar(iv,i,j,N-1);
+                imhdFluid.imhdVar(iv,i,j,N-1) = imhdFluid.imhdVar(iv,i,j,0);
+            }
+        }
+    }
 
+    // Walls
+    for (size_t k = 0; k < N; k++){
+        // i = 0 and i = N-1 yz-planes
+        for (size_t j = 0; j < N; j++){
+            imhdFluid.rho(0,j,k) = 7.0; // "Lithium"
+            imhdFluid.rho_u(0,j,k) = 0.0; // Rigid wall
+            imhdFluid.rho_v(0,j,k) = 0.0;
+            imhdFluid.rho_w(0,j,k) = 0.0;
+            imhdFluid.Bx(0,j,k) = 0.0; // Perfectly-conducting
+            imhdFluid.By(0,j,k) = 0.0;
+            imhdFluid.Bz(0,j,k) = 0.0;
+            imhdFluid.e(0,j,k) = imhdFluid.pressure(0,j,k) / (gamma - 1);
+        
+            imhdFluid.rho(N-1,j,k) = 7.0; // "Lithium"
+            imhdFluid.rho_u(N-1,j,k) = 0.0; // Rigid wall
+            imhdFluid.rho_v(N-1,j,k) = 0.0;
+            imhdFluid.rho_w(N-1,j,k) = 0.0;
+            imhdFluid.Bx(N-1,j,k) = 0.0; // Perfectly-conducting
+            imhdFluid.By(N-1,j,k) = 0.0;
+            imhdFluid.Bz(N-1,j,k) = 0.0;
+            imhdFluid.e(N-1,j,k) = imhdFluid.pressure(N-1,j,k) / (gamma - 1);
+        }
+        // j = 0 and j = N-1 xz-planes
+        for (size_t i = 0; i < N; i++){
+            imhdFluid.rho(i,0,k) = 7.0; // "Lithium"
+            imhdFluid.rho_u(i,0,k) = 0.0; // Rigid wall
+            imhdFluid.rho_v(i,0,k) = 0.0;
+            imhdFluid.rho_w(i,0,k) = 0.0;
+            imhdFluid.Bx(i,0,k) = 0.0; // Perfectly-conducting
+            imhdFluid.By(i,0,k) = 0.0;
+            imhdFluid.Bz(i,0,k) = 0.0;
+            imhdFluid.e(i,0,k) = imhdFluid.pressure(i,0,k) / (gamma - 1);
+        
+            imhdFluid.rho(i,N-1,k) = 7.0; // "Lithium"
+            imhdFluid.rho_u(i,N-1,k) = 0.0; // Rigid wall
+            imhdFluid.rho_v(i,N-1,k) = 0.0;
+            imhdFluid.rho_w(i,N-1,k) = 0.0;
+            imhdFluid.Bx(i,N-1,k) = 0.0; // Perfectly-conducting
+            imhdFluid.By(i,N-1,k) = 0.0;
+            imhdFluid.Bz(i,N-1,k) = 0.0;
+            imhdFluid.e(i,N-1,k) = imhdFluid.pressure(i,N-1,k) / (gamma - 1);
+        }
+    }
 }
 
 #endif
